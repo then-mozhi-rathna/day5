81d7599862dd7604aeccb23d9b5912c5d1aa897c
Thenmozhi Rathna <145723772+then-mozhi-rathna@users.noreply.github.com>
2024-07-03 12:38:23 +0530

    Update personclassdetails.js

---
+++ personclassdetails.js
@@ -0,0 +1,26 @@
+class person {
+    constructor(name, DOB, Age, address, phone number, email, fatherName, language) {
+      this.name = name;
+      this.DOB = DOB;
+      this.Age = Age;
+      this.address = address;
+      this.phone number = phone number;
+      this.email = email;
+      this.fatherName = fatherName;
+      this.language = language;
+    }
+}
+    const then = new Person (
+        "Rathna",
+        25/03/2001,
+        23,
+       " dindigul",
+        6846248923,
+        "thejjhhgsfjbhgej@gmail.com",
+       " john",
+        "Tamil and English"
+
+    );
+
+console.log(then();
+  
